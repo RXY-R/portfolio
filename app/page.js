417ab9b61c57c77b2590cdb34e8ea5b7e214fac3
RXY <45242634+RXY-R@users.noreply.github.com>
2024-11-23 16:40:04 +0600

    added animations

--- app/page.js
+++ app/page.js
@@ -6,7 +6,13 @@ import Skill from "./components/skill/skill";
 //#endregion
 
 //#region React hooks imports
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+//#endregion
+
+//#region Other imports
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { TextPlugin } from "gsap/TextPlugin";
 //#endregion
 
 //#region Theme Icons
@@ -48,21 +54,63 @@ const DarkIcon = () => (
 //#endregion
 
 export default function Home() {
-  const [theme, setTheme] = useState("light");
+  gsap.registerPlugin(ScrollTrigger);
 
-  const toggleTheme = () => {
-    if (theme === "light") {
+  //#region Theme
+  const [theme, setTheme] = useState("light");
+  useEffect(() => {
+    const prefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    if (prefersDark) {
       setTheme("dark");
       document.documentElement.setAttribute("data-theme", "dark");
-    } else {
-      setTheme("light");
-      document.documentElement.removeAttribute("data-theme");
     }
+  }, []);
+
+  const toggleTheme = () => {
+    theme === "light"
+      ? (setTheme("dark"),
+        document.documentElement.setAttribute("data-theme", "dark"))
+      : (setTheme("light"),
+        document.documentElement.removeAttribute("data-theme"));
   };
+  //#endregion
+
+  //#region Animations
+  const contentRef = useRef(null);
+  const scrambleTextRef = useRef(null);
+
+  useEffect(() => {
+    gsap.fromTo(
+      contentRef.current,
+      { filter: "blur(10px)", opacity: 0 },
+      { filter: "blur(0px)", opacity: 1, duration: 0.8, ease: "power2.in" }
+    );
+
+    const headings = document.querySelectorAll("section h1");
+    const paragraphs = document.querySelectorAll("section p");
+
+    [...headings, ...paragraphs].forEach((element) => {
+      gsap.fromTo(
+        element,
+        { opacity: 0 },
+        {
+          opacity: 1,
+          scrollTrigger: {
+            trigger: element,
+            start: "bottom bottom-=50",
+            toggleActions: "play none none reverse",
+          },
+        }
+      );
+    });
+  }, []);
+  //#endregion
 
   return (
     <div className="w-full min-h-screen flex items-center justify-center">
-      <div className="main_container">
+      <div ref={contentRef} className="main_container">
         <section className="intro_container">
           <div className="intro">
             <h1>Hi, I'm Angshu 👋</h1>
@@ -293,77 +341,73 @@ export default function Home() {
           </div>
         </section>
 
-        <nav className="w-full min-h-9 mb-2 fixed bottom-0 left-0 flex items-center justify-center">
-          <div className="flex gap-1 items-center select-none rounded-[12px] md:shadow-md max-h-9 bg-[#222222]">
-            <a onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}>
-              <div className="p-3">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  className="fill-white cursor-pointer"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                  <path d="M12.707 2.293l9 9c.63 .63 .184 1.707 -.707 1.707h-1v6a3 3 0 0 1 -3 3h-1v-7a3 3 0 0 0 -2.824 -2.995l-.176 -.005h-2a3 3 0 0 0 -3 3v7h-1a3 3 0 0 1 -3 -3v-6h-1c-.89 0 -1.337 -1.077 -.707 -1.707l9 -9a1 1 0 0 1 1.414 0m.293 11.707a1 1 0 0 1 1 1v7h-4v-7a1 1 0 0 1 .883 -.993l.117 -.007z" />
-                </svg>
-              </div>
-            </a>
-
-            <div className="w-[2px] h-9 bg-[#636363]"></div>
-
-            <a
-              onClick={() => window.scrollTo({ top: 388, behavior: "smooth" })}
-            >
-              <div className="p-3">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  className="fill-white cursor-pointer"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                  <path d="M3 12l3 3l3 -3l-3 -3z" />
-                  <path d="M15 12l3 3l3 -3l-3 -3z" />
-                  <path d="M9 6l3 3l3 -3l-3 -3z" />
-                  <path d="M9 18l3 3l3 -3l-3 -3z" />
-                </svg>
-              </div>
-            </a>
-
-            <a
-              onClick={() => window.scrollTo({ top: 650, behavior: "smooth" })}
-            >
-              <div className="p-3">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="fill-white cursor-pointer"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                  <path d="M21 3a1 1 0 0 1 0 2v9a3 3 0 0 1 -3 3h-5v2h2a1 1 0 0 1 0 2h-6a1 1 0 0 1 0 -2h2v-2h-5a3 3 0 0 1 -3 -3v-9a1 1 0 1 1 0 -2zm-4.293 4.293a1 1 0 0 0 -1.414 0l-2.293 2.292l-1.293 -1.292a1 1 0 0 0 -1.414 0l-3 3a1 1 0 0 0 0 1.414l.094 .083a1 1 0 0 0 1.32 -.083l2.293 -2.292l1.293 1.292a1 1 0 0 0 1.414 0l3 -3a1 1 0 0 0 0 -1.414" />
-                </svg>
-              </div>
-            </a>
-
-            <div className="w-[2px] h-9 bg-[#636363]"></div>
-
-            <a onClick={toggleTheme}>
-              <div className="p-3">
-                {theme === "light" ? <LightIcon /> : <DarkIcon />}
-              </div>
-            </a>
-          </div>
-        </nav>
-
         <br />
       </div>
+
+      <footer className="w-full min-h-9 mb-2 fixed bottom-0 left-0 flex items-center justify-center">
+        <nav className="flex gap-1 items-center select-none rounded-[12px] md:shadow-md max-h-9 bg-[#222222]">
+          <a onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}>
+            <div className="p-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="fill-white cursor-pointer"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M12.707 2.293l9 9c.63 .63 .184 1.707 -.707 1.707h-1v6a3 3 0 0 1 -3 3h-1v-7a3 3 0 0 0 -2.824 -2.995l-.176 -.005h-2a3 3 0 0 0 -3 3v7h-1a3 3 0 0 1 -3 -3v-6h-1c-.89 0 -1.337 -1.077 -.707 -1.707l9 -9a1 1 0 0 1 1.414 0m.293 11.707a1 1 0 0 1 1 1v7h-4v-7a1 1 0 0 1 .883 -.993l.117 -.007z" />
+              </svg>
+            </div>
+          </a>
+
+          <div className="w-[2px] h-9 bg-[#636363]"></div>
+
+          <a onClick={() => window.scrollTo({ top: 388, behavior: "smooth" })}>
+            <div className="p-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                className="fill-white cursor-pointer"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M3 12l3 3l3 -3l-3 -3z" />
+                <path d="M15 12l3 3l3 -3l-3 -3z" />
+                <path d="M9 6l3 3l3 -3l-3 -3z" />
+                <path d="M9 18l3 3l3 -3l-3 -3z" />
+              </svg>
+            </div>
+          </a>
+
+          <a onClick={() => window.scrollTo({ top: 650, behavior: "smooth" })}>
+            <div className="p-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="fill-white cursor-pointer"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M21 3a1 1 0 0 1 0 2v9a3 3 0 0 1 -3 3h-5v2h2a1 1 0 0 1 0 2h-6a1 1 0 0 1 0 -2h2v-2h-5a3 3 0 0 1 -3 -3v-9a1 1 0 1 1 0 -2zm-4.293 4.293a1 1 0 0 0 -1.414 0l-2.293 2.292l-1.293 -1.292a1 1 0 0 0 -1.414 0l-3 3a1 1 0 0 0 0 1.414l.094 .083a1 1 0 0 0 1.32 -.083l2.293 -2.292l1.293 1.292a1 1 0 0 0 1.414 0l3 -3a1 1 0 0 0 0 -1.414" />
+              </svg>
+            </div>
+          </a>
+
+          <div className="w-[2px] h-9 bg-[#636363]"></div>
+
+          <a onClick={toggleTheme}>
+            <div className="p-3">
+              {theme === "light" ? <LightIcon /> : <DarkIcon />}
+            </div>
+          </a>
+        </nav>
+      </footer>
     </div>
   );
 }
